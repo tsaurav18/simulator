4d6f9571a8efef5d10876ef23de0caf0bee9a7da
tsaurav18 <tsaurav1711@gmail.com>
2023-12-27 18:32:10 +0900

    fix simulation

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,6 @@
 import { persistReducer } from "redux-persist";
 import loginReducer from "./slices/loginSlice";
 import dataReducer from "./slices/UserResultSlice";
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
