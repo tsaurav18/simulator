aff6639ae2d4d07efbd38b51676a800625ef655d
tsaurav18 <tsaurav1711@gmail.com>
2024-01-04 14:00:10 +0900

    fix simulation

--- src/Pages/Results/Results.js
+++ src/Pages/Results/Results.js
@@ -1,6 +1,7 @@
 import React, {useEffect,useState} from "react";
+import "./Results.css"
 import MainNavbar from "../../Components/Navbar";
-import { Col, ShadowCol, Row } from "../../globalStyles/globalStyled";
+import { Col, ShadowCol, Row, ShadowRow } from "../../globalStyles/globalStyled";
 import Container from "react-bootstrap/Container";
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer, toast } from "react-toastify";
@@ -30,7 +31,7 @@ ChartJS.register(
 );
 const LineChart = ({data}) => {
   let simulator_title='';
-  // console.log("data>>>>>>>>>>>>>>>",data)
+
   let _borderColorList = [
     "rgb(53, 162, 235)",
     "rgb(255, 99, 132)",
@@ -75,33 +76,39 @@ const LineChart = ({data}) => {
     let invest_interval = parsed_data["invest_interval"] //분산 투자
     let model_type = parsed_data["model_type"] //예측 모델 
     let invest_inverval_dict = {"weekly":"주 단위", "daily":"일 단위"}
-    let filterLabel = `필터링:`
+    let filterLabel = `필터링: `
     if(r1_default===1){
-      filterLabel=  filterLabel+ ` R1, R2, R3, R4, R5,`
+      filterLabel=  filterLabel+ `R1, `
     }else if(r1_1===1){
-      filterLabel=filterLabel+ ` R1,`
+      filterLabel=filterLabel+ `R1.1, `
     }else if(r1_2===1){
-      filterLabel=filterLabel+ ` R2,`
+      filterLabel=filterLabel+ `R1.2, `
     }else if(r1_3===1){
-      filterLabel=filterLabel+ ` R3,`
+      filterLabel=filterLabel+ `R1.3, `
     }else if(r1_4===1){
-      filterLabel= filterLabel+ ` R4,`
+      filterLabel= filterLabel+ `R1.4, `
     }else if(r1_5===1){
-      filterLabel= filterLabel+ ` R5,`
+      filterLabel= filterLabel+ `R1.5, `
     }
+    if(r2===1){
+      filterLabel=filterLabel+ `R2, `
+    }if(r3===1){
+      filterLabel=filterLabel+ `R3, `
+    }
+
     if(remove_preferred===1){
-      filterLabel= filterLabel+ ` 우선주 제외,`
+      filterLabel= filterLabel+ `우선주 제외, `
     }
-    if(post_filtering!=null){
-      filterLabel = filterLabel + `${post_filtering}`
+    if(post_filtering!=="" && post_filtering!==null){
+      filterLabel = filterLabel + `${post_filtering}, `
     }
-    if(num_stocks!=""){
-      filterLabel= filterLabel+ `투자 종목 수: Top ${num_stocks}`
+    if(num_stocks!=="" && num_stocks!==null){
+      filterLabel= filterLabel+ `투자 종목 수: Top ${num_stocks}, `
     }
-    if(invest_interval!=""){
-      filterLabel= filterLabel+ `분산 투자: ${invest_inverval_dict[invest_interval]}`
+    if(invest_interval!==""){
+      filterLabel= filterLabel+ `분산 투자: ${invest_inverval_dict[invest_interval]}, `
     }
-    if(transaction_fee!=null){
+    if(transaction_fee!==null){
       filterLabel= filterLabel+ `수수료: ${transaction_fee}%`
     }
   // console.log("filterLabel", filterLabel)
@@ -134,11 +141,6 @@ const LineChart = ({data}) => {
     };
   });
 
-  // // Format the final chart data
-  // const modelsChartData = {
-  //   labels: datasets[0].data.map((point) => point.x),
-  //   datasets: datasets,
-  // };
 
   // Chart configuration
   const options = {
@@ -149,29 +151,38 @@ const LineChart = ({data}) => {
         display: false, // Set to false to remove data labels
       },
       zoom: {
-      
-      zoom: {
         pan: {
           enabled: true,
           mode: "x",
-         
         },
         zoom: {
           pinch: {
             enabled: true, // Enable pinch zooming
-         
           },
-          wheel: {
-            enabled: true, // Enable wheel zooming
-          },
+          // wheel: {
+          //   enabled: true, // Enable wheel zooming
+          // },
           mode: "x",
-          sensitivity: 0.1,
         },
       },
-    },
       legend: {
-        position: "right",
+        display:true,
+        position: "bottom",
+        align:"start", 
+        fullSize:true,
+        labels:{
+          font:{
+          size:16, 
+          color:"#000",
+          family:"sans-serif",
+          weight:"bold", 
+           
+        }}
+        
+        
       },
+    
+      
     },
   };
   return <Col style={{width:"100%", padding:"10px"}}>
@@ -184,10 +195,10 @@ const LineChart = ({data}) => {
 
 
 export default function Results() {
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
  const [loader, setLoader] = useState(false)
   const user_info_reducer = useSelector((state) => state.loginReducer);
-  const data_reducer = useSelector((state) => state.dataReducer);
+  // const data_reducer = useSelector((state) => state.dataReducer);
   
   const [resultDataList, setResultDataList] = useState([])
   console.log("resultDataList",resultDataList)
@@ -201,7 +212,7 @@ export default function Results() {
           // dispatch(saveDataState(JSON.parse(res.data)));
         }
         
-      } else if (res.status == 400) {
+      } else if (res.status === 400) {
         // toast("인터넷 장애로 인하여 정보를 못 가져왔습니다. 다시 로그인하세요.");
         const res = await resultAPI.fetchUserResult(user_info_reducer.user_id);
         if (res.status === 200) {
@@ -237,12 +248,34 @@ export default function Results() {
       
     }
   }, [])
-  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div>
       <MainNavbar />
-      <Container style={{ marginTop: 20 }}>
-      
+ 
+      <Container style={{ marginTop: 20}}>
+      <div className="filter_box" onClick={()=>handleClick()} style={{width:"100%"}}>
+       <div style={{ fontWeight:"700"}}> 필터 옾션 설명서</div>
+        {isOpen && <div style={{marginTop:"10px"}}>
+          
+          <p><span style={{fontWeight:"700"}}> R1:</span> <span>기본 세팅 전용 (R1.1, R1.2, R1.3, R1.4, R1.5)</span></p>
+          <p><span style={{fontWeight:"700"}}>R1.1: </span><span>시가총액 (억)</span></p>
+          <p><span style={{fontWeight:"700"}}>R1.2: </span><span>평균거래대금 (억)</span></p>
+          <p><span style={{fontWeight:"700"}}>R1.3:</span> <span>부채비율</span></p>
+          <p><span style={{fontWeight:"700"}}>R1.4:</span> <span>잔고율 </span></p>
+          <p><span style={{fontWeight:"700"}}>R1.5:</span> <span>비이슈종목</span></p>
+        
+          <p><span style={{fontWeight:"700"}}>R2:</span> <span>종복 섹터</span></p>
+          <p><span style={{fontWeight:"700"}}>R3: </span><span>종복 종목</span></p>
+          
+    
+          </div> }
+         
+        </div>
           {loader?  <Oval
                 height={50}
                 width={50}
@@ -258,9 +291,8 @@ export default function Results() {
                 strokeWidth={2}
                 strokeWidthSecondary={2}
               />:resultDataList?   <Row style={{width:"100%"}}>
-              <Col style={{ height:"100%"}}>{Object.keys(resultDataList).length>0 &&<Col>{Object.keys(resultDataList).map(k=><ShadowCol style={{ width:"900px", height:"100%", marginBottom:20}}>
-               
-                <LineChart data= {resultDataList[k]}/></ShadowCol>)}</Col>}</Col>
+              <Col style={{ height:"100%"}}>{Object.keys(resultDataList).length>0 &&<Col>{Object.keys(resultDataList).map(k=>
+                <ShadowCol style={{  height:"100%", marginBottom:20, backgroundColor:"#eee"}}>  <LineChart data= {resultDataList[k]}/></ShadowCol>)}</Col>}</Col>
               {/* <Col>model description</Col> */}
             </Row>:<Row>데이터가 없습니다. </Row>}
         
@@ -270,3 +302,7 @@ export default function Results() {
     </div>
   );
 }
+
+
+{/* <ShadowCol style={{ width:"700px", height:"100%", marginBottom:20}}> */}
+               
